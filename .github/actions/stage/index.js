40c44104feaba227e95559a18c58fa6c42b244a8
flower_elf <47104993+flower-elf@users.noreply.github.com>
2025-10-04 02:53:01 +0800

    Update index.js

--- .github/actions/stage/index.js
+++ .github/actions/stage/index.js
@@ -36,7 +36,7 @@ async function run() {
         args.push('--x86')
     if (arm)
         args.push('--arm')
-    await exec.exec('python', ['-m', 'pip', 'install', 'httplib2==0.22.0'], {
+    await exec.exec('python', ['-m', 'pip', 'install', 'httplib2'], {
         cwd: 'C:\\ungoogled-chromium-windows',
         ignoreReturnCode: true
     });
